d9fbafcb9f0b7fb3663d949c838b22da7e11541e
skevprog <kevin-s93@hotmail.com>
2022-04-01 10:42:39 -0300

    Apollo provider setup
    * Add query to fetch countries
    * Display countries in Home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes as Switch } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import Home from './pages/Home';
 import './App.css';
 
+const client = new ApolloClient({ uri: 'https://countries.trevorblades.com/', cache: new InMemoryCache() });
+
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/" element={<Home />} />
-        </Switch>
-      </Router>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route path="/" element={<Home />} />
+          </Switch>
+        </Router>
+      </div>
+    </ApolloProvider>
   );
 }
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import { gql, useQuery } from '@apollo/client';
 
-export default function index() {
+interface Country {
+  name: string;
+  code: string;
+  capital: string;
+}
+
+const GET_COUNTRIES = gql`
+  query GetCountries {
+        countries {
+          name
+          capital
+          code
+      }
+    }
+`;
+
+export default function Home() {
+  const { data } = useQuery(GET_COUNTRIES);
+
+  const renderCountries = (): JSX.Element | JSX.Element[] => {
+    if (data?.countries?.length) {
+      return data?.countries?.map((country: Country): JSX.Element => (<div key={country.code}><h2>{country.name}</h2></div>));
+    }
+    return <h2>No countries to show</h2>;
+  };
+
   return (
     <div>
-      <h1>This is the home</h1>
+      <h1>List of countries</h1>
+      {renderCountries()}
     </div>
   );
 }
